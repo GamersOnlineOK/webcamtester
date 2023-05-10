128bbad9673ca4c34384ba5f4b33d0495039df80
Leonardo De Los Santos <leo_dls@hotmail.com>
2023-05-10 16:36:01 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 import './App.css';
-import React from 'react';
-import Webcam from "react-webcam";
-import { useRef, useCallback } from 'react';
+import React, { useRef, useState } from "react";
 
 
 function App() {
-  const webcamRef = useRef(null);
+  const videoRef = useRef(null);
+  const [photoURL, setPhotoURL] = useState(null);
 
-  // const capture = useCallback(() => {
-  //   const imageSrc = webcamRef.current.getScreenshot();
-  //   // Enviar la imagen a la base de datos
-  // }, [webcamRef]);
-  const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: { exact: "environment" }
-  };
+  function handleCaptureClick() {
+    // Accedemos a la cámara y mostramos el stream de video en el elemento correspondiente
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+      
+    // Tomamos la foto
+    const canvas = document.createElement("canvas");
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    const context = canvas.getContext("2d");
+    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+    const dataUrl = canvas.toDataURL("image/png");
+    setPhotoURL(dataUrl);
+    console.log(photoURL);
+  }
 
   return (
-    <>
     <div>
-      <Webcam
-        audio={false}
-        height={720}
-        screenshotFormat="image/jpeg"
-        width={1280}
-        videoConstraints={videoConstraints}
-      >
-        {({ getScreenshot }) => (
-          <button
-            onClick={() => {
-              const imageSrc = getScreenshot()
-            }}
-          >
-            Capture photo
-          </button>
-        )}
-      </Webcam>
-      </div>
-    </>
+      <video ref={videoRef} width="640" height="480" autoPlay />
+      <button onClick={handleCaptureClick}>Capturar foto</button>
+      {photoURL && <img src={photoURL} alt="Captured photo" />}
+    </div>
   );
 }
 
