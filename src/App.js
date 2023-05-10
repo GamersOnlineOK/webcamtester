7fa84d4b2eece88ce01eb374ef666069ae65dfd4
Leonardo De Los Santos <leo_dls@hotmail.com>
2023-05-10 15:54:51 -0300

    testerCam

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Webcam from "react-webcam";
+import { useRef, useCallback } from 'react';
+
 
 function App() {
+  const webcamRef = useRef(null);
+
+  // const capture = useCallback(() => {
+  //   const imageSrc = webcamRef.current.getScreenshot();
+  //   // Enviar la imagen a la base de datos
+  // }, [webcamRef]);
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: { exact: "environment" }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>
+      <Webcam
+        audio={false}
+        height={720}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      >
+        {({ getScreenshot }) => (
+          <button
+            onClick={() => {
+              const imageSrc = getScreenshot()
+            }}
+          >
+            Capture photo
+          </button>
+        )}
+      </Webcam>
+      </div>
+    </>
   );
 }
 
